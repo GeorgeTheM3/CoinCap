ffee82dfd64e07c71804d07c541bc9a21339ca6d
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2022-10-13 12:19:15 +0300

    структура для парсинга json

---
+++ CoinCap/Model/Network/JSONModel/CryptoData.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  CoinCap
+//
+//  Created by Георгий Матченко on 13.10.2022.
+//
+
+import Foundation
+
+struct CryptoData: Decodable {
+    let data: [Coin]
+}
+
+struct Coin: Decodable {
+    let id: String
+    let rank: String
+    let symbol: String
+    let name: String
+    let priceUsd: String
+    let changePercent24Hr: String
+}
