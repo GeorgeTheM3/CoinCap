ae4633ff4e829e96d9a729e2e79f511ef6237887
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2022-10-13 12:38:41 +0300

    написал  в Network Manager метод fetch data

--- CoinCap/Model/Network/NetworkManager.swift
+++ CoinCap/Model/Network/NetworkManager.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class NetworkManager {
+    static let shared = NetworkManager()
     
+    func fetchData<T: Decodable>(url: String, model: T.Type, completion: @escaping (T) -> Void) {
+        guard let urlReuest = URL(string: url) else { return }
+        URLSession.shared.dataTask(with: urlReuest) { data, _, error in
+            if let error {
+                print(error.localizedDescription)
+            }
+            guard let data = data else { return}
+            do {
+                let result = try JSONDecoder().decode(model.self, from: data)
+                completion(result)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 }
